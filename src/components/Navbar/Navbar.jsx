57dd776e16b316cf0da6b8e11071b9f78d79752d
Ross Stephenson-Gill <rossstephensongill@gmail.com>
2022-08-22 15:23:41 +0100

    Fixed link to homepage for small devices

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -183,7 +183,7 @@ function Navbar() {
             variant="h5"
             noWrap
             component="a"
-            href=""
+            href="/"
             sx={{
               mr: 2,
               display: { xs: 'flex', md: 'none' },
